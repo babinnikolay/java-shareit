753f8c2df8d11b44a53adabcf456caecf44166c8
Babin Nikolay <ukolabrother@yandex.ru>
2022-09-05 18:29:25 +0300

    fix: have 2 Checkstyle violations

--- src/main/java/ru/practicum/shareit/user/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/UserStorage.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
