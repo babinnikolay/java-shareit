ae04183df431a3e7c590d417d6f5943bc2dc2e78
Babin Nikolay <ukolabrother@yandex.ru>
2022-09-18 14:53:04 +0300

    fix: lines covered ratio is 0.8, but expected minimum is 0.9; classes missed count is 2, but expected maximum is 1

---
+++ src/test/java/ru/practicum/shareit/unit/exception/ValidationErrorResponseTest.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.unit.exception;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.exception.ValidationErrorResponse;
+import ru.practicum.shareit.exception.Violation;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidationErrorResponseTest {
+    private ValidationErrorResponse validationErrorResponse;
+
+    @Test
+    void whenCreateValidationResponseThenGetIt() {
+        validationErrorResponse = new ValidationErrorResponse();
+        List<Violation> list = List.of(new Violation("name", "empty"));
+        validationErrorResponse.setViolations(list);
+        assertEquals(1, list.size());
+        assertEquals("empty", list.get(0).getMessage());
+    }
+
+}
