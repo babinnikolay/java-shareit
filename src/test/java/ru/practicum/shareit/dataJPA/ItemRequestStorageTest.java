949b6142999a45d339451c3c1ab9362e4e0697a0
Babin Nikolay <ukolabrother@yandex.ru>
2022-09-19 23:04:16 +0300

    fix: UnusedImports

--- src/test/java/ru/practicum/shareit/dataJPA/ItemRequestStorageTest.java
+++ src/test/java/ru/practicum/shareit/dataJPA/ItemRequestStorageTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
-import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.requests.ItemRequest;
