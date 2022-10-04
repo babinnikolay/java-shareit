6a88d8c4cb0fed15809e70925c159d8607dfc005
Babin Nikolay <ukolabrother@yandex.ru>
2022-10-04 21:42:35 +0300

    fix:UnusedImports

--- server/src/test/java/ru/practicum/shareit/webMVC/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/webMVC/ItemControllerTest.java
@@ -23,7 +23,6 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
